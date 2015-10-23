bb2f7ca57478c581f177ddc6948c2571e43a45d9
Cayo Costa Sobral <c.m.costa-sobral@warwick.ac.uk>
2015-10-23 13:32:41 +0100

    Created 'Hello World' application

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,8 +1,9 @@
 //! Main implementation of the mpags-cipher program
-
+#include <iostream>
 // This is one of the two valid signatures for main()
 int main(int /*argc*/, char* /*argv*/[]) {
-  // No requirement to return from main, but we do so for clarity and
-  // consistency with other functions.
+
+  std::cout << "Hello World!\n";
+
   return 0;
 }
